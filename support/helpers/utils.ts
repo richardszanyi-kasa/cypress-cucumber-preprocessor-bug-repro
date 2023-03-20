a0bbea96854ddfe6ded8e96e60403a1b7d38edfc
richardszanyi-kasa <101114202+richardszanyi-kasa@users.noreply.github.com>
2023-03-20 15:26:40 +0100

    Add repro

--- support/helpers/utils.ts
+++ support/helpers/utils.ts
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 import timezone from 'dayjs/plugin/timezone';
 import utc from 'dayjs/plugin/utc';
+import { fromNodeProviderChain } from '@aws-sdk/credential-providers';
 
 dayjs.extend(timezone);
 dayjs.extend(utc);
@@ -12,6 +13,8 @@ export class Utils {
   }
 
   isBoolean = (value: string) => value == 'true' ? true : false;
+
+
 }
 
 export const utils = new Utils();
