b3772c5f2d3cbcb624c6d5d20eda5f0614ec8226
richardszanyi-kasa <richard.szanyi@kasa.com>
2023-03-20 15:57:40 +0100

    Use the repro function in .feature file.

--- e2e/stepDefinitions/amazon/amazon.ts
+++ e2e/stepDefinitions/amazon/amazon.ts
@@ -1,5 +1,7 @@
 import { Given } from "@badeball/cypress-cucumber-preprocessor"
+import { utils } from "../../../support/helpers/utils";
 
 Given('Amazon is opened', () => {
   cy.visit('https://amazon.com')
+  cy.log(utils.getProvider())
 })
